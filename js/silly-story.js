499b9dee3b9655c059eb63b3e07e7a14f2f61574
SenkuDonda007 <kevadiyahet1234@gmail.com>
2023-09-29 12:03:55 -0400

    changes

--- js/silly-story.js
+++ js/silly-story.js
@@ -6,9 +6,9 @@
 - story for the paragraph that outputs the final story
 */
 
-var customName = document.getElementById("customname");
-var randomize = document.querySelector(".randomize");
-var story = document.querySelector(".story");
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
 
 
 /* STEP 3: Create the variable that contains the story string that will be modified 
@@ -16,8 +16,9 @@ var story = document.querySelector(".story");
 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 */
 
-var storyText =
-  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText =
+  "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
 
 
 
@@ -27,16 +28,16 @@ Area 51, Death Valley, Aruba
 spontaneously combusted, rapidly sublimated, evaporated instantly
 */
 
-var insertX = ["Donald Trump", "Jackie Chan", "Santa Claus"];
-var insertY = ["Area 51", "Death Valley", "Aruba"];
-var insertZ = ["spontaneously combusted", "rapidly sublimated", "evaporated instantly"];
+const insertX = ["Donald Trump", "Jackie Chan", "Santa Claus"];
+const insertY = ["Area 51", "Death Valley", "Aruba"];
+const insertZ = ["spontaneously combusted", "rapidly sublimated", "evaporated instantly"];
 
 
 // FUNCTIONS
 
 /* STEP 2: have a look at the following function - if you call this function and pass it an array, it will return one of the elements of that array randomly */
 function randomValueFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 /* STEP 6: Review the partially complete result() function below */
@@ -44,7 +45,7 @@ function result() {
     // STEP 7: Create a new variable called newStory and 
     //set it to the value of storyText - we don't want to overwrite the original story!
     
-    var newStory = storyText;
+    let newStory = storyText;
 
     /* STEP 8: Use the randomValueFromArray() function to generate a value for 
     each of three new variables - xItem, yItem, and zItem
@@ -52,9 +53,9 @@ function result() {
     which to grab a random string - for example if insertW was an array of strings, I would type:
 	var wItem = randomValueFromArray(insertW);*/
     
-    var xItem = randomValueFromArray(insertX);
-    var yItem = randomValueFromArray(insertY);
-    var zItem = randomValueFromArray(insertZ);
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
 
     //console.log(xItem);
     /* STEP 9: Replace the three placeholders in the newStory 
@@ -63,39 +64,39 @@ function result() {
     (with =). You might need to do one of the above replacements twice! */
     
 
-    newStory = newStory.replace(/:insertx:/g, xItem);
-    newStory = newStory.replace(/:inserty:/g, yItem);
-    newStory = newStory.replace(/:insertz:/g, zItem);
+  newStory = newStory.replace(/:insertx:/g, xItem);
+  newStory = newStory.replace(/:inserty:/g, yItem);
+  newStory = newStory.replace(/:insertz:/g, zItem);
 
     /* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
     
-    var customNameValue = customName.value;
-  if (customNameValue !== "") {
-    newStory = newStory.replace(/Bob/g, customNameValue);
-  }
+    if (customName.value.trim() !== "") {
+      newStory = newStory.replace(/Bob/g, customName.value);
+    }
 
     /* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
     if (document.getElementById("metric").checked) {
-        // STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
-            var weight = 300 * 0.453592;
+      // STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
+      const weightInPounds = 300;
+      const weightInKilograms = Math.round(weightInPounds * 0.453592);
 
         // STEP 11b: Replace the string 300 pounds with the updated weight in kg
-        newStory = newStory.replace("300 pounds", weight.toFixed(2) + " kilograms");
+    newStory = newStory.replace("300 pounds", `${weightInKilograms} kilograms`);
 
         // STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
-        var fahrenheit = 94;
-    var celsius = ((fahrenheit - 32) * 5) / 9;
+        const tempInFahrenheit = 94;
+    const tempInCelsius = Math.round(((tempInFahrenheit - 32) * 5) / 9);
 
         // STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
-        newStory = newStory.replace("94 fahrenheit", celsius.toFixed(2) + " Celsius");
+        newStory = newStory.replace("94 farenheit", `${tempInCelsius} celsius`);
   }
-    }
+
     /* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
     story.textContent = newStory;
 
     // The following line makes the paragraph visible
     story.style.visibility = "visible";
-
+}
 
 // EVENT LISTENERS
 /* STEP 5: Add a click event listener to the randomize variable 
